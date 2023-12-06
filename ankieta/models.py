eec38a236f26e16520733a9ad95651db68ca90c0
GrzegorzNiewierowski <gniewierowski@gmail.com>
2023-12-06 23:18:16 +0100

    Add validation for values

--- ankieta/models.py
+++ ankieta/models.py
@@ -1,9 +1,21 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class Ankieta(models.Model):
-    wiek = models.PositiveSmallIntegerField()
-    wzrost = models.DecimalField(decimal_places=2, max_digits=3)
+    wiek = models.PositiveSmallIntegerField(
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ])
+    wzrost = models.DecimalField(
+        decimal_places=2,
+        max_digits=3,
+        validators=[
+            MaxValueValidator(2.5),
+            MinValueValidator(1.2)
+        ]
+    )
     płeć = models.CharField(
         max_length=1,
         choices=[
