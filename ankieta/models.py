0d44682ce7592f131791e2c505bb8a402a2206d2
GrzegorzNiewierowski <gniewierowski@gmail.com>
2023-12-06 21:28:28 +0100

    Ankieta model

--- ankieta/models.py
+++ ankieta/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Ankieta(models.Model):
+    wiek = models.PositiveSmallIntegerField()
+    wzrost = models.DecimalField(decimal_places=2, max_digits=3)
+    płeć = models.CharField(
+        max_length=1,
+        choices=[
+            ('M', 'Mężczyzna'),
+            ('K', 'Kobieta')
+        ]
+    )
+    ulubiony_kolor = models.CharField(max_length=64)
+
+
+
+
+
+
